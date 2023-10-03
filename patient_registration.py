b5e004b47e1d7bbc86877297dc4de42ba932bb65
brightigiemokha <brightigiemokha@yahoo.com>
2023-10-03 19:14:42 +0000

    innitial commit

---
+++ patient_registration.py
@@ -0,0 +1,18 @@
+from tkinter import*
+from tkinker import tkinter
+import random
+import time
+import datetime
+from tkinter import messagebox
+from mysql.connector
+
+
+class Hospital:
+    def __init__(self,root):
+        self.root=rootself.root.title("Hospital Management System")
+        self.root.geometry("1540x800+0+0")
+
+
+root=Tk()
+ob=Hospital(root)
+root.mainloop()
