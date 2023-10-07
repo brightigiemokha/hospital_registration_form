55951c63f3a1403c798e7e3c13e2628701a90768
brightigiemokha <brightigiemokha@yahoo.com>
2023-10-07 11:54:05 +0000

    add login setting and frames

--- patient_registration.py
+++ patient_registration.py
@@ -1,18 +1,32 @@
 from tkinter import*
-from tkinker import tkinter
-import random
+import tkinker.messagebox
+from tkinter import ttk
 import time
 import datetime
-from tkinter import messagebox
-from mysql.connector
+import random
 
 
-class Hospital:
-    def __init__(self,root):
-        self.root=rootself.root.title("Hospital Management System")
-        self.root.geometry("1540x800+0+0")
+def main():
+    root = TK()
+    app = Windowl(root)
 
+class Windowl:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Pharmacy Management Systems")
+        self.master.geometry('135x750+0+0')
+        self.frame = Frame (self.master)
+        self.frame.pack()
 
-root=Tk()
-ob=Hospital(root)
-root.mainloop()
+        
+self.LabelTitle =Label(self.frame, text ='hospital Management System', font=('arial',50, 'bold'),bd=20,)
+self.LabelTitle.grid(row=0, column=0, columnspan=2, pady=20)
+
+self.Loginframe1 = Frame(self.frame,width=1010,height=300, bd=20,relief='ridge')
+self.Loginframe1.grid(row=1, column=0)
+
+self.Loginframe2 = Frame(self.frame,width=1000,height=100,bd=20,relief='ridge')
+self.Loginframe2.grid(row=2, column=0)
+
+self.Loginframe3 = Frame(self.frame,width=1000,height=200,bd=20,relief='ridge')
+self.Loginframe3.grid(row=3, column=0,pady=2)
